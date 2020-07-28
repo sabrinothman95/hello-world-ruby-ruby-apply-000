522055e96a5e47d84242e0dd3e1f10df83c01774
Sabrin Othman <sabrinothman95@gmail.com>
2020-07-28 16:36:56 +0000

    Done.

---
+++ hello_world.rb
@@ -0,0 +1,24 @@
+puts ("hello_world")
+
+puts 'hello_world.rb'
+puts'Hello World!'
+puts'ruby hello_world.rb'
+
+
+puts $rubyhello_world.rb
+hello_world!
+
+print 'learn submit'
+
+
+
+
+
+
+
+
+
+
+
+
+
